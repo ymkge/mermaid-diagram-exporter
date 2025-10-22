ada1a7cfade3ca8edd110bd588b883b3808444b3
ymkge <39545752+ymkge@users.noreply.github.com>
2025-10-22 21:29:32 +0900

    debug: add @sparticuz/chromium@141.0.0

--- app/api/generate-pdf/route.ts
+++ app/api/generate-pdf/route.ts
@@ -22,7 +22,7 @@ export async function POST(request: Request) {
     } else {
       browser = await puppeteer.launch({
         args: chromium.args,
-        executablePath: await chromium.createPath(),
+        executablePath: await chromium.executablePath(),
         headless: true,
       });
     }
