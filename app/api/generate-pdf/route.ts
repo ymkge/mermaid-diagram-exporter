c77d9d6b5651bc16ea05b28f02e1c7cb31c66848
ymkge <39545752+ymkge@users.noreply.github.com>
2025-10-24 23:09:22 +0900

    debug: chromium.executablePahth use

--- app/api/generate-pdf/route.ts
+++ app/api/generate-pdf/route.ts
@@ -22,7 +22,7 @@ export async function POST(request: Request) {
     } else {
       browser = await puppeteer.launch({
         args: chromium.args,
-        executablePath: './lib/mmdc-bin/chromium/chromium',
+        executablePath: await chromium.executablePath(),
         headless: true,
       });
     }
